3ab9f116950fd22673fdc188251b8c07f724bc85
Cécil Millerioux <cecil.millerioux@gmail.com>
2019-07-03 14:29:39 +0200

    ajout des fichiers ScoresController.java, Scores.java et ScoreRepository.java

---
+++ src/main/java/com/wildcodeschool/bandersnatch/controllers/ScoresController.java
@@ -0,0 +1,37 @@
+package com.wildcodeschool.bandersnatch.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+import com.wildcodeschool.bandersnatch.entities.Scores;
+import com.wildcodeschool.bandersnatch.repositories.ScoresRepository;
+
+import java.util.List;
+
+@Controller
+@ResponseBody
+public class ScoresController {
+
+    @GetMapping("/scores")
+    public List<Scores> getScores(@RequestParam(defaultValue = "%") String scores) {
+        return ScoresRepository.selectByNickname(scores);
+    }
+
+@PostMapping("/scores")
+@ResponseStatus(HttpStatus.CREATED)
+public Scores store(
+    @RequestParam String nickname,
+    @RequestParam (defaultValue = "0") int user_score
+    ) {
+    int idGeneratedByInsertion = ScoresRepository.insert(
+        nickname,
+        user_score
+    );
+    return ScoresRepository.selectById(idGeneratedByInsertion);
+    }
+}
