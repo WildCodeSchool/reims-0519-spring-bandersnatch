15594d381164695832dcbfbfe1d7f8c328f0b038
Dupuis Sebastien <sebastien-dupuis@laposte.net>
2019-07-03 10:33:14 +0200

    Ajout de la class Gamecontroller

--- src/main/java/com/wildcodeschool/bandersnatch/controllers/GameController.java
+++ src/main/java/com/wildcodeschool/bandersnatch/controllers/GameController.java
@@ -4,40 +4,50 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-class PageController {
+class GameController {
 
-    @GetMapping("/")
-    public String index(Model model) {
-        return "index";
+
+
+    @PostMapping("/game")
+    @ResponseBody
+    public String game(HttpSession session, Model model, @RequestParam(required=false) String nickname, @RequestParam(required=false) String action) {
+        if(session.getAttribute("nickname") == null) {
+            // first time we get here : check nickname
+            if(nickname == null || nickname.equals("")) {
+                // invalid nickname : go back to home page
+                return "redirect: /";
+            }
+
+            // register nickname in the session
+            session.setAttribute("nickname", nickname);
+        }
+
+        if(action != null) {
+            // we're playing : check the current action
+            if(action.equals("win")) {
+                session.setAttribute("win", true);
+            }
+            if(action.equals("lose")) {
+                
+            }
+        }
+
+        if(session.getAttribute("win") != null && session.getAttribute("win").equals(true)) {
+            // we won : end of game
+            return "win";
+        }
+
+        // we're still playing : show the current choice
+        model.addAttribute("nickname", nickname);
+        return 
+            "<form action='/game' method='post'><input name='action' value='win'><button class='btn btn-primary my-1'>Get out of prison!</button></form>" +
+            "<form action='/game' method='post'><input name='action' value='lose'><button class='btn btn-primary my-1'>Get out of prison!</button></form>";
+        //return "game";
     }
 
-    @GetMapping("/game")
-    public String game(Model model) {
-        return "game";
-    }
-
-    @GetMapping("/result")
-    public String result(Model model) {
-        return "result";
-    }
-
-    @RequestMapping("/game")
-public class gamertag {
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String affichername(final ModelMap pModel, 
-                @RequestParam(value="nickname") final String pnickname) {
-
-        pModel.addAttribute("nickname", pnickname);
-        return "name";
-    }
 }
-} 
