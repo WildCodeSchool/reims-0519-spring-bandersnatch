283f54b424a3a05344811ed15e7753c4f63e7003
cecil millerioux <cecil.millerioux@gmail.com>
2019-07-04 17:01:10 +0200

    add entities, repository, controller of score

---
+++ src/main/java/com/wildcodeschool/bandersnatch/entities/Score.java
@@ -0,0 +1,26 @@
+package com.wildcodeschool.bandersnatch.entities;
+
+public class Score {
+
+    private int id;
+    private String nickname;
+    private int user_score;
+
+    public Score(int id, String nickname, int user_score) {
+        this.id = id;
+        this.nickname = nickname;
+        this.user_score = user_score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getUser_score() {
+        return user_score;
+    }
+}
