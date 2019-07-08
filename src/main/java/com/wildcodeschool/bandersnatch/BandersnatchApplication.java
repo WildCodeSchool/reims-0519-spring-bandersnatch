ac3c23874be4d0d6e0eb623526f645881ca5d14b
Furkan Celik <c.furkan.contact@gmail.com>
2019-07-08 15:24:25 +0200

    cleaning code

--- src/main/java/com/wildcodeschool/bandersnatch/BandersnatchApplication.java
+++ src/main/java/com/wildcodeschool/bandersnatch/BandersnatchApplication.java
@@ -1,16 +1,12 @@
 package com.wildcodeschool.bandersnatch;
-
 import com.wildcodeschool.bandersnatch.repositories.ScoreRepository;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class BandersnatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BandersnatchApplication.class, args);
-		
+		SpringApplication.run(BandersnatchApplication.class, args);	
 	}
 
 }
