500cd0420cb0fce1ef23f95bbe0ba92ddcecd806
Vincent Guegan <gueganvincent@gmail.com>
2019-07-08 15:00:13 +0200

    add rooms 9 & 10

--- src/main/java/com/wildcodeschool/bandersnatch/repositories/RoomRepository.java
+++ src/main/java/com/wildcodeschool/bandersnatch/repositories/RoomRepository.java
@@ -1,7 +1,6 @@
 package com.wildcodeschool.bandersnatch.repositories;
 
 import com.wildcodeschool.bandersnatch.entities.Room;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,41 +9,36 @@ public class RoomRepository {
     private List<Room> rooms;
 
     public RoomRepository() {
-        this.rooms = new ArrayList<Room>();
-        rooms.add(new Room(1, "You just arrived in the Room of Torture, lucky you :) Your new friend was waiting for you, his name is « Romano el CodMaster », he wants to keep your eyes open with giant forks, in front of a kind of « shiny window », with weird text written on it, for hours and for his own enjoyment. You need to escape as soon as possible!!!  The door you came by is now locked, 2 choices in front of you. Hurry up, pick a door!!!"));
-        
-        rooms.add(new Room(2, "You are in the Room of Remus. Someone his running all around this room with a weird big ball looking like a bean… His name is Nikolaï Huranoff, he wants you to play with him. Big and strong, you don’t want to offend him and you say yes, otherwise the risk is your head looks like the ball. After few hours, you’re exhausted and want to rest but Nikolaï does not…never... Quick!!! Run from this crazy guy, you have 2 possibilities, because your previous door you came by his now locked. Hurry up, take a pick!!!"));
-        
-        rooms.add(new Room(3, "You arrived in a new room: The Tattoo Room. After the door closes behind you, a strange man called Bryun caVANSny wants absolutely to tattoo you . You're nice so you say yes for a black darkhorse tatoo on your harm, but after 1 hour tattoo, you see you're getting a weird pink unicorn... Time to stop this mindness and leave, you need to pick one: left or right!!!"));
-        
-        rooms.add(new Room(4, "You just arrived in the Veggie Room. Inside, you meet a strange woman, dressed like poisonIvy in Batman (I hope you saw the movie…), she his cooking a weird smelly soup in her chaudron; she looks nice and wants to share her diner with you; It is really nice of her but the smell and color are making you cry, and you would have tasted it but dead rats nearby the chaudron make you think otherwise… Time to leave!!! Quick, take one of the 2 doors, she is coming at you with a spoon!!!"));
-        
-        rooms.add(new Room(5, "You are in the Inception Room. In this room, all your senses are put to the test: the room is turning at 360°, chairs, tables, tools, everything is turning like in a washing machine... If you want to survive, avoid everything in your path and try to catch one of the 2 doors at the other side of the room: right or left!!!!"));
-        
-        rooms.add(new Room(6, "You arrive in the Nintendo Room, a Room full of Nintendo games, coolll!!! You meet the switch Master, Stephano Mario, he loves games, especially Pokemon and wants you to play with him and you accept, only 5 minutes and you leave. After checking your watch you see you played for 3 hours... Time to leave your new friend. Pick a door: right or left."));
-        
-        rooms.add(new Room(7, "You just arrived in a bedroom, nice room and you hear someone is getting out of the shower. Oh my god, it's the Camilla Room!!! it's Camilla Parker Bowles, and she is loosing a towel on the floorrrr.... Your eyes are burning and you're crying, you don't want to see this... Close your eyes and try to find a door: right or left!!!!"));
-        
-        rooms.add(new Room(8, "Yes, you went out!!!! Great :) Your friends Sherlock & Watson are here. Now get your ass out of that city and thanks for playing :)"));
 
+        this.rooms = new ArrayList<Room>();
+
+        rooms.add(new Room(1, "The Torture Room<br><br>You just arrived in the Room of Torture, lucky you :)<br><br>Your new friend was waiting for you, his name is « Romano el CodMaster », he wants to keep your eyes open with giant forks, in front of a kind of « shiny window », with weird text written on it, for hours and for his own enjoyment.<br><br>You need to escape as soon as possible!!!<br><br>The door you came by is now locked, 2 choices in front of you. Hurry up, pick a door!!!"));
+        rooms.add(new Room(2, "The Remus Room<br><br>Someone his running all around this room with a weird big ball looking like a bean…<br><br>His name is Nikolaï Huranoff, he wants you to play with him. Big and strong, you don’t want to offend him and you say yes, otherwise the risk is your head looks like the ball.<br><br>After few hours, you’re exhausted and want to rest but Nikolaï does not…never...<br><br>Quick!!! Run from this crazy guy, you have 2 possibilities, because your previous door you came by his now locked. Hurry up, take a pick!!!"));
+        rooms.add(new Room(3, "The Tattoo Room<br><br>After the door closes behind you, a strange man called Bryun caVANSny wants absolutely to tattoo you.<br><br>You're nice so you say yes for a black darkhorse tatoo on your harm, but after 1 hour tattoo, you see you're getting a weird pink unicorn...<br><br>Time to stop this mindness and leave, you need to pick one: left or right!!!"));
+        rooms.add(new Room(4, "The Veggie Room<br><br>Inside, you meet a strange woman, dressed like poisonIvy in Batman (I hope you saw the movie…), she his cooking a weird smelly soup in her chaudron;<br>she looks nice and wants to share her diner with you;<br>It is really nice of her but the smell and color are making you cry, and you would have tasted it but dead rats nearby the chaudron make you think otherwise…<br><br>Time to leave!!! Quick, take one of the 2 doors, she is coming at you with a spoon!!!"));
+        rooms.add(new Room(5, "The Inception Room<br><br>In this room, all your senses are put to the test: the room is turning at 360°, chairs, tables, tools, everything is turning like in a washing machine...<br><br>If you want to survive, avoid everything in your path and try to catch one of the 2 doors at the other side of the room: right or left!!!!"));
+        rooms.add(new Room(6, "The Nintendo Room<br><br>A Room full of Nintendo games, coolll!!!<br><br>You meet the switch Master, Stephano Mario, he loves games, especially Pokemon and wants you to play with him and you accept, only 5 minutes and you leave.<br><br>After checking your watch you see you played for 3 hours...<br><br>Time to leave your new friend. Pick a door: right or left."));
+        rooms.add(new Room(7, "The Camilla Room<br><br>You just arrived in a bedroom, nice room and you hear someone is getting out of the shower.<br><br>Oh my god, it's the Camilla Room!!!<br><br>It's Camilla Parker Bowles, and she is loosing a towel on the floorrrr....<br><br>Your eyes are burning and you're crying, you don't want to see this...<br><br>Close your eyes and try to find a door: right or left!!!!"));
+        rooms.add(new Room(8, "The Ball Trap Room<br><br>In this room, you see a lot of plates fixed on the walls and crushed on the floor. Then you hear a detonation!!!!! This is the Ball Trap Hunter shooting, Cecilio Millerio. He is tired of shooting plates and your presence is making a little more fun for his hunting game. If you don't want to be his trophy, run fast and open one of the 2 doors at the end of the room"));
+        rooms.add(new Room(9, "The GoT Room<br><br>You're arriving in a simple room with a TV, a coach and 3 people sitten on it, the 2 other doors are behind the TV so you need to cross the people. But they are speaking of the last episode of the last season of Game Of Thrones...Unfortunately, you didn't see it and you DON'T want to hear what's happening... Hurry up, close your ears and get out of here!!!!"));
+        rooms.add(new Room(10, "Exit<br><br>Yes, you went out!!!! Great :)<br><br>Your friends Sherlock & Watson are here.<br><br>Now get your ass out of that city and thanks for playing :)"));
     }
 
     public List<Room> getRooms() {
 
-
-
-        return room;
+        return rooms;
     }
 
+    
     public Room getRoomById(int id) {
-        for(Room room : room) {
+
+        for(Room room : rooms) {
 
             if(room.getId() == id) {
 
                 return room;
             }
         }
-
         return null;
     }
 }
