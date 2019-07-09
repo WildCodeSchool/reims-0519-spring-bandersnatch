d6bae4507736c046b8ef6fc48a782422d281c7e8
Furkan Celik <c.furkan.contact@gmail.com>
2019-07-09 10:29:23 +0200

    cleaning code

--- src/main/java/com/wildcodeschool/bandersnatch/controllers/ScoreController.java
+++ src/main/java/com/wildcodeschool/bandersnatch/controllers/ScoreController.java
@@ -2,11 +2,7 @@ package com.wildcodeschool.bandersnatch.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
 
 import com.wildcodeschool.bandersnatch.entities.Score;
 import com.wildcodeschool.bandersnatch.repositories.ScoreRepository;
@@ -20,17 +16,5 @@ public class ScoreController {
     @ResponseBody
     public List<Score> getScores() {
         return ScoreRepository.selectAll();
-    }
-
-    @PostMapping("/scores")
-    public String store(
-        @RequestParam String nickname,
-        @RequestParam (defaultValue = "0") int user_score
-    ) {
-        int idGeneratedByInsertion = ScoreRepository.insert(
-            nickname,
-            user_score
-        );
-        return "redirect:/scores";
     }
 }
